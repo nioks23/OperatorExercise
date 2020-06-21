58edb9f290fdd4144506dc7eb03fa4c8c1643a20
NIOKHOBAYE DIOUF <niokhobayediouf@MacBook-Pro.local>
2020-06-21 15:58:50 -0400

    Exercise done

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var div = a / b;
+            var mod = a % b;
+            Console.WriteLine($"{a}/{b} is { div} remainder {mod}");
+
+            Console.WriteLine("What is the radius of your circle");
+
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalculateArea(radius);
+
+            
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}");
+            
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+            
+        }
+
+
+    }
+}
