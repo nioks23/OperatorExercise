8fd61210efe052c06c2df1133927b641f49d72d6
NIOKHOBAYE DIOUF <niokhobayediouf@MacBook-Pro.local>
2020-06-22 09:44:05 -0400

    Exerice with notes

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,6 +4,14 @@ namespace OperatorExercise
 {
     class MainClass
     {
+        // Method called here will be used below 
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+
+        }
+
         public static void Main(string[] args)
         {
             var a = 17;
@@ -14,25 +22,25 @@ namespace OperatorExercise
             var i = 3;
             var k = ++i * j++;
 
-            
             Console.WriteLine($"{a}/{b} is { div} remainder {mod} and K is equal to {k}");
 
             Console.WriteLine("What is the radius of your circle?");
 
-            var radius = double.Parse(Console.ReadLine());
+            //  The line below makes the user enter a value for the radius
+            var radius = double.Parse(Console.ReadLine()); 
+
+            // this line calculates the area based on the method above and the radius entered above                                           
             var areaOfCircle = CalculateArea(radius);
 
-            
-
             Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}");
-            
+
+
+
+
+
         }
 
-        public static double CalculateArea(double radius)
-        {
-            return Math.PI * (radius * radius);
-            
-        }
+        
 
 
     }
